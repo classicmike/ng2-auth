38be9884472ac1e11beb27dc3eacab1fa44a3a2c
Michael Tran <koramaiku@Michaels-iMac.local>
2017-01-16 00:19:44 +1100

    Finished App

---
+++ app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Auth } from '../../services/auth.service';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'home',
+  templateUrl: 'home.component.html',
+})
+
+
+export class HomeComponent  { 
+    constructor(private auth: Auth){
+        
+    }    
+}
